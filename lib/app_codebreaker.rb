e55ad7b5491a1484a5ff3e7763babdbc5210eb79
MorpheusFR <Creatorsk@gmail.com>
2018-07-25 18:01:48 +0300

    building

--- lib/app_codebreaker.rb
+++ lib/app_codebreaker.rb
@@ -16,13 +16,13 @@ class AppCodebreaker
   def initialize(env)
     @request = Rack::Request.new(env)
     @game = Codebreaker::Game.new
+    # @game_console = Codebreaker::GameConsole.new
     @secret_code = @game.code_view_with_hint
     @path_to_file = PATH_TO_LOG_FILES
     @status = false
     @player_name = nil
-    @total_attempts = ATTEMPTS
-    @hints = HINTS
-    @hints_used = 0
+    @total_attempts = total_attempts
+    @match_result = match_result
   end
 
   def response
@@ -31,64 +31,108 @@ class AppCodebreaker
       @request.session.clear
       Rack::Response.new(render('index'))
     when '/index' then Rack::Response.new(render('game_session'))
+    when '/game'
+      start unless @request.session[:coregame]
+      Rack::Response.new(render('game'))
     when '/game_session'
       start_game unless @request.session[:game]
       Rack::Response.new(render('game_session'))
-    when '/submit_guess'
-      submit_guess(@request.params['guess'])
+    when '/submit_code'
+      submit_code(@request.params['player_code'])
+      Rack::Response.new { |response| response.redirect('/game_session') }
+    when '/hints'
+      hints
       Rack::Response.new { |response| response.redirect('/game') }
-    # when '/show_hint'
-    #   show_hint
-    #   Rack::Response.new { |response| response.redirect('/game') }
-    # when '/enter_name'
-    #   enter_name(@request.params['name'])
-    #   Rack::Response.new { |response| response.redirect('/best_results') }
-    # when '/best_results'
-    #   load_results
-    #   Rack::Response.new(render('best_results.html.erb'))
+    when '/enter_name'
+      enter_name(@request.params['name'])
+      Rack::Response.new { |response| response.redirect('/score') }
+    when '/score'
+      load_results
+      Rack::Response.new(render('score'))
     else Rack::Response.new(render('404'), 404)
     end
   end
 
   private
 
+  def start
+    @game = Codebreaker::Game.new
+    @secret_code = @game.code_view_with_hint
+    @request.session[:coregame] = @game
+  end
+
+  def submit_code(input_code='1111')
+    @request.session[:coregame].validate_turn(input_code)
+
+    # player_result = @request.session[:coregame].match_result
+    # @request.session[:coregame].validate_turn(input_code)
+    # @request.session[:game].result_on_input_data(user_input)
+    # @request.session[:attempts] << { input_code: input_code, player_result: player_result }
+  end
+
+  def hints
+    @request.session[:coregame].get_a_hint
+  end
+
+  def secret_code
+    @secret_code
+  end
+
+  def input_code
+    @request.params[player_code]
+  end
+
+  def validate_turn
+    @request.session[:coregame].validate_turn(@request.params[input_code])
+    @match_result = @request.session[:match_result]
+    @request.session[:coregame].counter_attepmpts_and_turn
+  end
+
+  def match_result
+    @request.session[:total_attempts]
+  end
+
+  def total_attempts
+    @request.session[:total_attempts]
+  end
+
   # def load_results
   #   @results = YAML.load(RgHwCodebreaker::ResultsAccessor.load_results_file)
   #   @results_table_header = @results.shift
   # end
 
-  def start_game
-    @player_name = @request.params[player_name]
-    @request.session[:game] = Codebreaker::Game.new
-    @request.session[:player_name] = @request.params[player_name]
-    @request.session[:game].code_view_with_hint
-    @request.session[:attempts] = []
-    @request.session[:show_hint] = false
-  end
+  # def start_game
+  #   @player_name = @request.params[player_name]
+  #   @request.session[:game] = Codebreaker::Game.new
+  #   @request.session[:player_name] = @request.params[player_name]
+  #   @request.session[:game].code_view_with_hint
+  #   @request.session[:attempts] = []
+  #   @request.session[:hint]
+  # end
 
-  def submit_guess(guess)
-    if @request.session[:game].valid_guess?(guess)
-      guess_result = @request.session[:game].check_guess(guess)
-      @request.session[:attempts] << { guess: guess, guess_result: guess_result }
-    else
-      @request.session[:error_msg] = 'Your input is invalid'
-    end
-  end
+  # def submit_guess(input_code)
+  #   if @request.session[:game].validate_turn(input_code)
+  #     player_result = @request.session[:game].match_result
+  #     @request.session[:attempts] << { input_code: input_code, player_result: player_result }
+  #   else
+  #     @request.session[:error_msg] = 'Your input is invalid'
+  #   end
+  # end
 
-  def enter_name(player_name)
-    current_result = [player_name, Date.today, 10 - @request.session[:game].turns]
-    RgHwCodebreaker::ResultsAccessor.write_result_to_file(current_result)
-    @request.session[:notice_msg] = 'Your result saved'
-  end
+  # def enter_name(player_name)
+  #   current_result = [player_name, Date.today, 10 - @request.session[:game].turns]
+  #   Codebreaker::ResultsAccessor.write_result_to_file(current_result)
+  #   @request.session[:notice_msg] = 'Your result saved'
+  # end
 
-  def hint
-    @request.session[:hint] = true
-    @request.session[:hint] = if @request.session[:game].any_hints_left?
-                                "Hint: #{@request.session[:game].give_a_hint}***"
-                              else
-                                'No hints left :('
-                              end
-  end
+  # def hint
+  #   @request.session[:hint] = true
+  #   @request.session[:hint] = if @request.session[:game].any_hints_left?
+  #                               "Hint: #{@request.session[:game].get_a_hint}***"
+  #                             else
+  #                               'No hints left :('
+  #                             end
+  # end
 
   def render(template)
     path = File.expand_path("../views/#{template}.html.haml", __FILE__)
